f9a14bdb1a14568aeb209bbe06b75ea938f9a611
HP <HP@172.16.154.146>
2024-02-06 17:32:35 +0800

    Initial Commit

---
+++ buteelt/src/buteelt/lab1.java
@@ -0,0 +1,19 @@
+package buteelt;
+import java.util.*;
+
+public class lab1 {
+	public static void main (String [] args) {
+		int a, b, s;
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner (System.in);
+		a = scan.nextInt();
+	    b = scan.nextInt();
+	    s = a + b;
+	    System.out.println("niilber :" + s);
+	    
+
+		
+	    
+		
+	}
+}
