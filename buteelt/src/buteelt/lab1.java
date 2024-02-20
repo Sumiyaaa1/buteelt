c13c344934654df0538b24907fe0c716757aa9a4
HP <HP@172.16.154.161>
2024-02-20 17:06:44 +0800

    05:06

--- buteelt/src/buteelt/lab1.java
+++ buteelt/src/buteelt/lab1.java
@@ -4,12 +4,11 @@ import java.util.*;
 public class lab1 {
 	public static void main (String [] args) {
 		int a, b, s;
-		@SuppressWarnings("resource")
-		Scanner scan = new Scanner (System.in);
-		a = scan.nextInt();
-	    b = scan.nextInt();
+		
+		new Scanner(System.in).nextInt();
+		
 	    s = a + b;
-	    System.out.println("niilber :" + s);
+	    System.out.println("niilber :" + s[]);
 	    
 
 		
